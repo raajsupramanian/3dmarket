bf26f428b996c7e0e5236811d00ca3b1ef2b912b
Pawan Darda <pawan.darda@autodesk.com>
2015-12-09 04:52:16 -0500

    services

---
+++ store/services.py
@@ -0,0 +1,59 @@
+import os
+import json
+import requests
+
+CLIENT_ID='ZVu6cFFGxMJohgsaDsbLfu9jNs4pQEif'
+CLIENT_SECRET='47C2LzDqv9b7C120'
+
+def get_apigee_token():
+    access_token = None
+    req = requests.post("https://developer.api.autodesk.com/authentication/v1/authenticate",
+                        data={'client_id': CLIENT_ID,
+                              'client_secret': CLIENT_SECRET,
+                              'grant_type': 'client_credentials'
+                             }, verify=False)
+
+    if req.status_code == 200:
+        access_token = req.json()['access_token']
+
+    return access_token
+
+def create_oss_bucket(bucket):
+    print "getting token"
+    access_token = get_apigee_token()
+
+    if not access_token :
+        return '{"token_failed"}'
+
+    req = requests.post("https://developer.api.autodesk.com/oss/v2/buckets",
+                       headers={"Authorization": "Bearer %s" % access_token,
+                                "Content-Type": "application/json"},
+                       data= json.dumps({"bucketKey" : bucket, "policyKey" : "transient"}) , verify=False)
+
+    if req.status_code != 200:
+        return '{"create_failed"}'
+    else:
+        return req.json()
+
+def upload_oss_obj(bucket,local_file, oss_obj_name):
+
+    access_token = get_apigee_token()
+
+    if not access_token :
+        return '{"token_failed"}'
+
+    with open(local_file, 'rb') as file_to_be_upload:
+        req = requests.put("https://developer.api.autodesk.com/oss/v2/buckets/%s/objects/%s"
+                           % (bucket, oss_obj_name),
+                           headers={"Authorization": "Bearer %s" % access_token,
+                                    "Content-Type": "application/octet-stream"},
+                           data=file_to_be_upload, verify=False)
+        if req.status_code != 200:
+            return '{"upload_failed"}'
+        else:
+            return req.json()
+
+if __name__ == "__main__":
+
+    v =  create_oss_bucket("hupahupa")
+    print v
