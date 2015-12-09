0ec232ef559dc42fb2dc3f420ec762c07c0a9f0a
Raaj Supramanian M <raajsupramanian@raajs-mbp.ads.autodesk.com>
2015-12-09 16:41:08 +0800

    models added

---
+++ market/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Store(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    class Meta:
+        db_table = "store"
+
+class Products(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    price = models.CharField(max_length=200)
+    display_image = models.CharField(max_length=200)
+    oss_url = models.CharField(max_length=200)
--- market/urls.py
+++ market/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from views import CreateStoreView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -7,6 +8,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name="index.html"), name='homepage'),
+    url(r'^createshop', CreateStoreView.as_view(), name='shop_create'),
     # Examples:
     # url(r'^$', 'market.views.home', name='home'),
     # url(r'^market/', include('market.foo.urls')),
