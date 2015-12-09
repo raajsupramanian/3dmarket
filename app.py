4e5dc99fb9f5a57da763ea6c38f494ba9ebbf86c
Raaj Supramanian M <raajsupramanian@raajs-mbp.ads.autodesk.com>
2015-12-09 11:50:05 +0800

    adding redis

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/user/<username>')
+def user_details(username):
+    return "Your username is " + username
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
