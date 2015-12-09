dfb3ccf1ca2924aec5407ef578c374e88861098e
Raaj Supramanian M <raajsupramanian@raajs-mbp.ads.autodesk.com>
2015-12-09 12:12:25 +0800

    index page addition and static serving

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, send_from_directory, render_template
+app = Flask(__name__, static_url_path='/static/')
 app.debug = True
+
+@app.route('/static/<path:path>')
+def send_js(path):
+    return send_from_directory('assets', path)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
 @app.route('/user/<username>')
 def user_details(username):
